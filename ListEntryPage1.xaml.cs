a03f09022bc79e0a30b4afb87cb5ef5250786349
Betea Bianca Mariana <bianca.betea@stud.ubbcluj.ro>
2023-11-27 19:54:32 +0200

    Tot lab ul 8

---
+++ ListEntryPage1.xaml.cs
@@ -0,0 +1,33 @@
+using Betea_Bianca_Lab7.Models;
+
+namespace Betea_Bianca_Lab7;
+
+public partial class ListEntryPage1 : ContentPage
+{
+	public ListEntryPage1()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+}
