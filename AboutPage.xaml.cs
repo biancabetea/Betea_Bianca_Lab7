9952a819f0903ba5368e7a0c3e7633df23fdad42
Betea Bianca Mariana <bianca.betea@stud.ubbcluj.ro>
2023-11-27 19:13:48 +0200

    Lab1 fara erori

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Betea_Bianca_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
--- AboutPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Betea_Bianca_Lab7;
-
-public partial class AboutPage1 : ContentPage
-{
-	public AboutPage1()
-	{
-		InitializeComponent();
-	}
-}
